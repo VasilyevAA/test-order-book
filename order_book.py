92c0c92d8174569a8c74862af086071c2376605d
VAA <a.vasiliev@wert.io>
2020-11-30 23:34:44 +0300

    add and implement SortedDefaultDict small refactor order class (miss trading_pair, fix static field, add curr_volume)

--- order_book.py
+++ order_book.py
@@ -1,17 +1,23 @@
 import time
 import uuid
 from decimal import Decimal
-from collections import UserList, OrderedDict, defaultdict
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from functools import total_ordering
+from sortedcontainers import SortedDict
+from collections import UserList, OrderedDict
 
 
-class OrderedDefaultDict(OrderedDict, defaultdict):
+class SortedDefaultDict(SortedDict):
     def __init__(self, default_factory=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.default_factory = default_factory
 
+    def __missing__(self, key):
+        result = self[key] = self.default_factory()
+        return result
 
-class OrderAction:
+
+class OrderType:
     ASK = 1
     BID = 2
 
@@ -23,15 +29,35 @@ class OrderStatus:
     CLOSE = "close"
 
 
+# @total_ordering
 @dataclass
 class Order:
     price: Decimal
     volume: Decimal
     type: str
     owner_id: str
-    __status: str = 'init'
-    __id: str = uuid.uuid4()
-    __timestamp: int = time.time_ns()
+    trading_pair: str
+    __status: str = OrderStatus.INIT
+    __curr_volume: Decimal = None
+    __id: str = field(default_factory=lambda: str(uuid.uuid4()))
+    __timestamp: int = field(default_factory=time.time_ns)
+
+    def __post_init__(self):
+        if not isinstance(self.price, Decimal) or not isinstance(self.volume, Decimal):
+            raise Exception("Price and Volume should be Decimal instance")
+        if self.price <= 0:
+            raise Exception(f"Try to create invalid order. Set invalid {self.price=}")
+        if self.volume <= 0:
+            raise Exception(f"Try to create invalid order. Set invalid {self.volume=}")
+
+        self.__curr_volume = self.volume
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return other == self.id
+        if isinstance(other, self.__class__):
+            return self.id == other.id
+        return False
 
     @property
     def id(self):
@@ -45,13 +71,8 @@ class Order:
     def status(self):
         return self.__status
 
-    def __post_init__(self):
-        if not isinstance(self.price, Decimal) or not isinstance(self.volume, Decimal):
-            raise Exception("Price and Volume should be Decimal instance")
-        if self.price <= 0:
-            raise Exception(f"Try to create invalid order. Set invalid {self.price=}")
-        if self.volume <= 0:
-            raise Exception(f"Try to create invalid order. Set invalid {self.volume=}")
+    def early_than(self, order: "Order"):
+        return self.ts < order.ts
 
 
 class OrderList(UserList):
